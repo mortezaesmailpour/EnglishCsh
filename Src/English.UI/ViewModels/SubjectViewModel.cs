b4c5415d3a599b0941a37accf0c75791e8f1b8cc
Morteza <mailto:morteza.esmailpour@amplicade.com>
2022-09-08 18:29:11 +0430

    IsSingular checkbox was added

--- Src/English.UI/ViewModels/SubjectViewModel.cs
+++ Src/English.UI/ViewModels/SubjectViewModel.cs
@@ -1,7 +1,26 @@
+using English.Persons;
+
 namespace English.UI.ViewModels;
 
 public class SubjectViewModel : BaseViewModel
 {
+
+    private bool _isSingular;
+    public bool IsSingular
+    {
+        get => _isSingular;
+        set
+        {
+            var person = SelectedSubject.BaseSubject.Person;
+            _ = value
+                ? person == Person.First ? SelectedSubject = Subjects[1] : person == Person.Third ? SelectedSubject = Subjects[6] : SelectedSubject = Subjects[2]
+                : person == Person.First ? SelectedSubject = Subjects[0] : person == Person.Third ? SelectedSubject = Subjects[5] : SelectedSubject = Subjects[2];
+            SetField(ref _isSingular, value);
+        }
+    }
+
+
+    //================
     public ObservableCollection<SubjectModel> Subjects { get; init; }
     private SubjectModel _selectedSubject;
     public SubjectModel SelectedSubject
